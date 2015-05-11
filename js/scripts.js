2617b529c743727b602daeb4e9e5a92bdb0bbae7
SilentImp <thesilentimp@gmail.com>
2015-05-11 18:36:00 +0300

    Update 2015-05-11T15:35:58.263Z

--- js/scripts.js
+++ js/scripts.js
@@ -200,7 +200,7 @@ book = (function() {
     if (Modernizr.mq('(min-width: ' + this.one_page_width + 'px)') && this.page_number >= this.page_count - 3) {
       return;
     }
-    if (Modernizr.mq('(max-width: ' + this.one_page_width + 'px) and (min-width: 801px)') && this.page_number >= this.page_count - 2) {
+    if (Modernizr.mq('(max-width: ' + this.one_page_width + 'px) and (min-width: 801px)') && this.page_number >= this.page_count - 1) {
       return;
     }
     if (Modernizr.mq('(max-width: 800px)') && (this.page_number >= this.page_count - 2)) {
